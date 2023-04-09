93446da95efcdd0db96648c55586a9ac594a9c52
Midiy <midiy009@mail.ru>
2023-04-09 03:27:21 +0300

    Added scheduler implementation.

---
+++ WebCrawler/WebCrawler.Core/Schedulers/Scheduler.WorkerActions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebCrawler.Core.Interfaces;
+using WebCrawler.Core.Interfaces.Models;
+using WebCrawler.Core.Models;
+
+namespace WebCrawler.Core.Schedulers
+{
+    // В этой части класса описаны методы, работающие в воркер-потоках.
+    public partial class Scheduler
+    {
+        private static readonly TimeSpan WorkersDelay = TimeSpan.FromMilliseconds(10);
+
+        private static async Task WorkerActionAsync(int workerId, WorkersSharedState sharedState)
+        {
+            CancellationToken cancellationToken = sharedState.CancellationToken;
+            IMultiReaderQueue<IQueuedUrl> queue = sharedState.Queue;
+            ISchedulerSettings settings = sharedState.Settings;
+
+            IWebDownloader webDownloader = settings.WebDownloaderFactory.Create();
+            IContentSaver contentSaver = settings.ContentSaverFactory.Create();
+
+            bool isIdle = false;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // Пытаемся увеличить число запросов, сделанных за эту секунду.
+                // Если не получается - значит, лимит исчерпан, ждём и пробуем ещё раз.
+                while (!sharedState.TryIncrementRequestsCount())
+                {
+                    await Task.Delay(WorkersDelay);
+                    if (cancellationToken.IsCancellationRequested)
+                        return;
+                }
+
+                // Пытаемся получить следующий элемент очереди для текущего воркера.
+                // Если не получилось - не думаем, что работа закончилась, возможно,
+                // остальные воркеры ещё положат в очередь элементы, которые этому надо будет обрабатывать. 
+                if (!queue.TryDequeue(workerId, out IQueuedUrl? queuedUrl))
+                {
+                    // Если воркер ещё не в состоянии простоя - переводим его в него и сообщаем, что количество простаивающих воркеров увеличилось на 1.
+                    if (!isIdle)
+                    {
+                        isIdle = true;
+                        sharedState.IncrementIdleWorkersCount();
+                    }
+
+                    // Если все воркеры в данный момент простаивают - это может значить, что работа действительно закончилась, но не обязательно -
+                    // возможно, какой-то воркер успел подложить этому работы и уйти в простой уже после того, как мы проверили, что для этого воркера работы нет.
+                    // Чтобы исключить такую ситуацию, дополнительно проверяем, что не только все воркеры простаивают, но и очередь пуста.
+                    // Мы проверяем пустоту очереди только в тех случаях, когда все воркеры простаивают, потому что для ConcurrentMultiReaderQueue эта операция полностью блокирующая,
+                    // а значит - дорогая, т.к. заставит ждать всех остальных воркеров. Но если они и так (предположительно) ничего не делают - то можно.
+                    // Если очередь действительно пуста - завершаем работу этого воркера и сообщаем, что количество завершивших работу воркеров увеличилось на 1.
+                    if (sharedState.IsAllWorkersIdle() && queue.IsEmpty())
+                    {
+                        sharedState.IncrementFinishedWorkersCount();
+                        return;
+                    }
+
+                    // Если кто-то ещё работает или в очереди есть данные - ждём и снова проверяем, нет ли для этого воркера работы.
+                    await Task.Delay(WorkersDelay);
+                    continue;
+                }
+
+                // Если для воркера нашлась работа, и он до этого простаивал - выводим его из состояния простоя и сообщаем,
+                // что количество простаивающих воркеров уменьшилось на 1.
+                if (isIdle)
+                {
+                    isIdle = false;
+                    sharedState.DecrementIdleWorkersCount();
+                }
+
+                Uri url = queuedUrl.Url;
+                if (!webDownloader.TryGetPageContent(url, out IPageContent pageContent))
+                {
+                    // Если по какой-то причине не получилось загрузить содержимое страницы -
+                    // проверяем, что мы её ещё не слишком много раз пробовали загрузить, и если нет - возвращаем в очередь.
+                    if (queuedUrl.RequestsCount < sharedState.Settings.MaxRetries)
+                        sharedState.Queue.TryEnqueue(new QueuedUrl(url, queuedUrl.RequestsCount + 1));
+                    continue;
+                }
+
+                sharedState.Queue.TryEnqueueMany(pageContent.Links.Select(link => new QueuedUrl(link)));
+                _ = contentSaver.TrySaveContent(url, pageContent.TextContent);
+            }
+        }
+
+        private static async Task TimerActionAsync(PeriodicTimer timer, WorkersSharedState sharedState)
+        {
+            while (!sharedState.CancellationToken.IsCancellationRequested)
+            {
+                await timer.WaitForNextTickAsync();
+                sharedState.ResetRequestsCount();
+            }
+        }
+
+        private static async Task QueueSnapshotActionAsync(WorkersSharedState sharedState)
+        {
+            ISchedulerSettings settings = sharedState.Settings;
+            IMultiReaderQueue<IQueuedUrl> queue = sharedState.Queue;
+
+            PeriodicTimer timer = new(settings.QueueSnapshotSavePeriod);
+            IMultiReaderQueueSnapshotSaver<IQueuedUrl> queueSnapshotSaver = settings.QueueSnapshotSaverFactory.Create();
+
+            while (!sharedState.CancellationToken.IsCancellationRequested)
+            {
+                await timer.WaitForNextTickAsync();
+                IMultiReaderQueueSnapshot<IQueuedUrl> queueSnapshot = queue.ExportQueueSnapshot();
+                _ = queueSnapshotSaver.TrySaveSnapshot(queueSnapshot);
+            }
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler.Core/Schedulers/Scheduler.WorkersSharedState.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Threading;
+using WebCrawler.Core.Collections;
+using WebCrawler.Core.Comparers;
+using WebCrawler.Core.Interfaces.Models;
+using WebCrawler.Core.Interfaces;
+
+namespace WebCrawler.Core.Schedulers
+{
+    // В этой части класса описан вспомогательный класс, содержащий данные, совместно используемые воркер-потоками.
+    public partial class Scheduler
+    {
+        /// <summary>
+        /// Вспомогательный класс, описывающий данные, совместно используемые воркерами, которые запускает планировщик.
+        /// </summary>
+        private class WorkersSharedState : IDisposable
+        {
+            public event Action AllWorkersHaveFinished;
+
+            private CancellationTokenSource _cancellationTokenSource = new();
+            private int _requestsCount = 0;
+            private int _idleWorkersCount = 0;
+            private int _finishedWorkersCount = 0;
+
+            /// <summary>
+            /// Очередь URL-адресов страниц для загрузки.
+            /// </summary>
+            public IMultiReaderQueue<IQueuedUrl> Queue { get; set; }
+
+            /// <summary>
+            /// Настройки планировщика запросов.
+            /// </summary>
+            public ISchedulerSettings Settings { get; }
+
+            /// <summary>
+            /// Токен прерывания операций.
+            /// </summary>
+            public CancellationToken CancellationToken
+            {
+                get => _cancellationTokenSource.Token;
+            }
+
+            /// <summary>
+            /// Конструктор.
+            /// </summary>
+            /// <param name="settings">Настройки планировщика запросов.</param>
+            public WorkersSharedState(ISchedulerSettings settings)
+            {
+                Settings = settings;
+                Queue = new ConcurrentMultiReaderQueue<IQueuedUrl>(settings.NumberOfWorkers, QueuedUrlEqualityComparer.Instance);
+            }
+
+            /// <summary>
+            /// Метод, пытающийся увеличить количество сделанных запросов на 1.
+            /// </summary>
+            /// <returns>
+            /// Если количество запросов достигло предела, задаваемого <see cref="ISchedulerSettings.NumberOfWorkers"/> - 
+            /// <see langword="false"/>, иначе - <see langword="true"/>.
+            /// </returns>
+            /// <remarks>Количество сделанных запросов увеличивается потокобезопасно и неблокирующим образом.</remarks>
+            public bool TryIncrementRequestsCount()
+            {
+                int currentValue;
+                int exchangedValue;
+                do
+                {
+                    currentValue = _requestsCount;
+                    if (currentValue >= Settings.RequestsPerSecond)
+                        return false;
+
+                    exchangedValue = Interlocked.CompareExchange(ref _requestsCount, currentValue + 1, currentValue);
+                } while (exchangedValue != currentValue);
+                return true;
+            }
+
+            /// <summary>
+            /// Метод, сбрасывающий количество сделанных запросов.
+            /// </summary>
+            public void ResetRequestsCount()
+            {
+                _requestsCount = 0;
+            }
+
+            /// <summary>
+            /// Метод, атомарно инкрементирующий количество простаивающих воркеров.
+            /// </summary>
+            public void IncrementIdleWorkersCount()
+            {
+                Interlocked.Increment(ref _idleWorkersCount);
+            }
+
+            /// <summary>
+            /// Метод, атомарно декрементирующий количество простаивающих воркеров.
+            /// </summary>
+            public void DecrementIdleWorkersCount()
+            {
+                Interlocked.Decrement(ref _idleWorkersCount);
+            }
+
+            /// <summary>
+            /// Метод, проверяющий что все воркеры простаивают.
+            /// </summary>
+            /// <returns>Если все воркеры простаивают - <see langword="true"/>, иначе - <see langword="false"/>.</returns>
+            public bool IsAllWorkersIdle()
+            {
+                return _idleWorkersCount == Settings.NumberOfWorkers;
+            }
+
+            /// <summary>
+            /// Метод, инкрементирующий количество воркеров, закончивших работу.
+            /// </summary>
+            public void IncrementFinishedWorkersCount()
+            {
+                Interlocked.Increment(ref _finishedWorkersCount);
+                if (_finishedWorkersCount == Settings.NumberOfWorkers)
+                    AllWorkersHaveFinished?.Invoke();
+            }
+
+            /// <summary>
+            /// Метод, устанавливающий <see cref="CancellationToken"/> в состояние запроса прерывания операции.
+            /// </summary>
+            public void RequestCancellation()
+            {
+                _cancellationTokenSource.Cancel();
+            }
+
+            /// <summary>
+            /// Метод, сбрасывающий состояние <see cref="WorkersSharedState"/>.
+            /// </summary>
+            /// <remarks>Этот метод не затрагивает ни настройки планировщика, ни текущее состояние очереди.</remarks>
+            public void ResetState()
+            {
+                ResetRequestsCount();
+                _idleWorkersCount = 0;
+                _finishedWorkersCount = 0;
+                _cancellationTokenSource.Dispose();
+                _cancellationTokenSource = new CancellationTokenSource();
+            }
+
+            /// <summary>
+            /// Метод, освобождающий используемые неуправляемые ресурсы.
+            /// </summary>
+            public void Dispose()
+            {
+                _cancellationTokenSource.Dispose();
+            }
+        }
+    }
+}
