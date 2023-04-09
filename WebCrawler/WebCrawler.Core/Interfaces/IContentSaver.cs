ae1898a68f3a994e777bd0882b4ac473b4036664
Midiy <midiy009@mail.ru>
2023-04-09 03:25:23 +0300

    Added interfaces for scheduler.

---
+++ WebCrawler/WebCrawler.Core/Interfaces/IContentSaver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebCrawler.Core.Interfaces
+{
+    /// <summary>
+    /// Интерфейс сущности, сохраняющей текстовое содержимое Web-страниц.
+    /// </summary>
+    public interface IContentSaver
+    {
+        /// <summary>
+        /// Метод, пытающийся сохранить текстовое содержимое, соответствующий указанному URL-адресу.
+        /// </summary>
+        /// <param name="url">URL-адрес, которому соответствует текстовое содержимое <paramref name="content"/>.</param>
+        /// <param name="content">Текстовое содержимое, которое требуется сохранить.</param>
+        /// <returns>Если удалось сохранить содержимое - <see langword="true"/>, иначе - <see langword="false"/>.</returns>
+        bool TrySaveContent(Uri url, string content);
+    }
+}
---
+++ WebCrawler/WebCrawler.Core/Interfaces/Models/ISchedulerSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WebCrawler.Core.Interfaces.Models
+{
+    /// <summary>
+    /// Интерфейс настроек планировщика запросов.
+    /// </summary>
+    public interface ISchedulerSettings
+    {
+        /// <summary>
+        /// Количество загрузчиков, работающих одновременно.
+        /// </summary>
+        int NumberOfWorkers { get; }
+
+        /// <summary>
+        /// Максимальное суммарное количество запросов в секунду.
+        /// </summary>
+        int RequestsPerSecond { get; }
+
+        /// <summary>
+        /// Максимальное количество повторных попыток запросить страницу.
+        /// </summary>
+        int MaxRetries { get; }
+
+        /// <summary>
+        /// Частота сохранения снапшота очереди.
+        /// </summary>
+        TimeSpan QueueSnapshotSavePeriod { get; }
+
+        /// <summary>
+        /// Фабрика загрузчиков содержимого Web-страниц.
+        /// </summary>
+        IFactory<IWebDownloader> WebDownloaderFactory { get; }
+
+        /// <summary>
+        /// Фабрика сущностей, сохраняющих содержимое Web-страниц.
+        /// </summary>
+        IFactory<IContentSaver> ContentSaverFactory { get; }
+
+        /// <summary>
+        /// Фабрика сущностей, сохраняющих снапшоты очереди.
+        /// </summary>
+        IFactory<IMultiReaderQueueSnapshotSaver<IQueuedUrl>> QueueSnapshotSaverFactory { get; }
+    }
+}
