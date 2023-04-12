64134bd6d307cb85ef9ae435fda90ad35fe3dbf4
Midiy <midiy009@mail.ru>
2023-04-12 03:23:09 +0300

    Added usage demo.

--- WebCrawler/WebCrawler.CLI/Program.cs
+++ WebCrawler/WebCrawler.CLI/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using WebCrawler.Core.Factories;
+using WebCrawler.Core.Interfaces.Models;
+using WebCrawler.Core.Models;
+using WebCrawler.Core.Resolvers;
+using WebCrawler.Core.Schedulers;
 
 namespace WebCrawler.CLI
 {
@@ -6,7 +14,80 @@ namespace WebCrawler.CLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // TODO: Добавить возможность нормально настраивать сбор - через аргументы командной строки или интерактивно.
+            SubdomainOnlyUrlResolver urlResolver = new();
+            HtmlWebDownloaderFactory webDownloaderFactory = new(urlResolver);
+            string timestampString = DateTime.UtcNow.ToString("yyyy-MM-ddTHH_mm_ss");
+            string outputFilePath = $"./test_output/content_{timestampString}.csv";
+            ConcurrentCsvContentSaverCachingFactory contentSaverFactory = new(outputFilePath);
+            TimestampFilePathTemplate filePathTemplate = new("./test_output/", "QueueSnapshot_{0}.snp");
+            FileMultiReaderQueueSnapshotSaverFactory<IQueuedUrl> snapshotSaverFactory = new(filePathTemplate);
+
+            SchedulerSettings schedulerSettings = new(NumberOfWorkers: 100,
+                                                      RequestsPerSecond: 1000,
+                                                      MaxRetries: 2,
+                                                      QueueSnapshotSavePeriod: SchedulerSettings.DefaultQueueSnapshotSavePeriod,
+                                                      WebDownloaderFactory: webDownloaderFactory,
+                                                      ContentSaverFactory: contentSaverFactory,
+                                                      QueueSnapshotSaverFactory: snapshotSaverFactory);
+            using Scheduler scheduler = new(schedulerSettings);
+
+            string currentDirectory = Path.TrimEndingDirectorySeparator(Directory.GetCurrentDirectory());
+            // Дерево страниц для университетского сайта очень большое, это займёт кучу времени!
+            //scheduler.AddUrls(new Uri("https://spbu.ru"));
+            scheduler.AddUrls(new Uri("https://metanit.com/"));
+
+            DateTime startTime = default;
+            bool isFinished = false;
+            scheduler.AllPagesCollected += () =>
+            {
+                TimeSpan sessionDuration = DateTime.Now - startTime;
+
+                IMultiReaderQueueSnapshot<IQueuedUrl> snapshot = scheduler.ExportQueueSnapshot();
+                long downloadedCount = CountLines(outputFilePath);
+                Console.WriteLine($"Все страницы загружены. Всего посещено {snapshot.AlreadyProcessedEntries.Count} страниц, загружено {downloadedCount} страниц. Текущий сеанс длился {sessionDuration}.");
+                isFinished = true;
+            };
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+
+                scheduler.Stop();
+
+                TimeSpan sessionDuration = DateTime.Now - startTime;
+                IMultiReaderQueueSnapshot<IQueuedUrl> snapshot = scheduler.ExportQueueSnapshot();
+                snapshotSaverFactory.Create().TrySaveSnapshot(snapshot);
+
+                int queueSize = snapshot.Buckets.Aggregate(0, (acc, curr) => acc + curr?.Length ?? 0);
+                int visitedCount = snapshot.AlreadyProcessedEntries.Count - queueSize;
+                long downloadedCount = CountLines(outputFilePath);
+                Console.WriteLine($"Досрочное прерывание. В очереди {queueSize} адресов, посещено {visitedCount} страниц, загружено {downloadedCount} страниц. Текущий сеанс длился {sessionDuration}.");
+                isFinished = true;
+            };
+
+            startTime = DateTime.Now;
+            scheduler.Start();
+            while (!isFinished)
+            {
+            }
+
+            Console.ReadKey();
+        }
+
+        private static long CountLines(string fileName)
+        {
+            long result = 0;
+            try
+            {
+                using StreamReader reader = new(fileName);
+                while (reader.ReadLine() is not null)
+                    result++;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            return result;
         }
     }
 }
